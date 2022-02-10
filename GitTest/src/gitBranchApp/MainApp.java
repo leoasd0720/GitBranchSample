063fcd6b34f1cba333d9b624b1dd6946cb0c1556
leoasd <leodevil13@gmail.com>
2022-02-10 09:34:08 +0800

    1st Version

---
+++ GitTest/src/gitBranchApp/MainApp.java
@@ -0,0 +1,12 @@
+package gitBranchApp;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 這是組長第一次編輯
+		System.out.println("1st Version");
+
+	}
+
+}

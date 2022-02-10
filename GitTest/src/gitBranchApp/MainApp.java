9e36642ea5ecc953938ad3573c4bf5c06c5ca98e
leoasd <leodevil13@gmail.com>
2022-02-10 09:46:38 +0800

    add Math Message

--- GitTest/src/gitBranchApp/MainApp.java
+++ GitTest/src/gitBranchApp/MainApp.java
@@ -6,7 +6,15 @@ public class MainApp {
 		// TODO Auto-generated method stub
 		// 這是組長第一次編輯
 		System.out.println("1st Version");
-
+		// 這是家數學類別版本
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 5));
 	}
+}
 
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
